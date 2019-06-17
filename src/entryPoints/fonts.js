a275f8923ffda18e792a629c3504f5bb3085323d
Manuel Gutierrez <manu.gtzp@gmail.com>
2019-06-17 11:21:19 -0500

    Fonts

---
+++ src/entryPoints/fonts.js
@@ -0,0 +1,12 @@
+import "../fonts/Lora/lora-regular.eot";
+import "../fonts/Lora/lora-regular.ttf";
+import "../fonts/Lora/lora-regular.woff";
+import "../fonts/Lora/lora-regular.woff2";
+
+import "../fonts/Raleway/raleway-regular.eot";
+import "../fonts/Raleway/raleway-regular.ttf";
+import "../fonts/Raleway/raleway-regular.woff";
+import "../fonts/Raleway/raleway-regular.woff2";
+
+import "../styles/lora.css";
+import "../styles/raleway.css";
